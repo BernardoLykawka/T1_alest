9acee749eab7251d179bd21e61bebf6e0f957951
lu4mic <luizafm7@gmail.com>
2024-09-30 15:12:28 -0300

    lista feita do linked stack

--- LinkedStack.java
+++ LinkedStack.java
@@ -1,39 +1,71 @@
+import java.util.NoSuchElementException;
+
 /**
  * (2 Pontos) Uma classe LinkedStack, contendo uma implementação completa de uma
  * estrutura do tipo Pilha que utilize como estrutura base alguma das classes de Lista
  * Encadeada que implementamos em aula, ou seja, sua classe deve instanciar um objeto
  * do tipo SinglyLinkedList ou DoublyLinkedList e armazenar os valores nesta estrutura
  */
+class Node {
+    int element;
+    Node next;
 
+    public Node(int element) {
+        this.element = element;
+        this.next = null;
+    }
+}
+//next aponta pro elemento inferior na pilha
 public class LinkedStack implements StackTAD {
+    private Node topo;
+    private int count;
 
+    public LinkedStack() {
+        this.topo = null;
+        this.count = 0;
+    }
     @Override
     public void push(int element) {
-
+        Node valor = new Node(element);
+        if (this.topo == null) {
+            this.topo = valor;
+        }else{
+            valor.next = this.topo;
+            this.topo = valor;
+        }
+        this.count++;
     }
 
     @Override
     public int pop() {
-        return 0;
+        if (this.topo == null) {
+            throw new NoSuchElementException();
+        }
+        Node valor = this.topo;
+        topo = valor.next;
+        this.count--;
+
+        return valor.element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return count;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return count==0;
     }
 
     @Override
     public void clear() {
-
+        this.topo = null;
+        count = 0;
     }
 
     @Override
     public int top() {
-        return 0;
+        return topo.element;
     }
 }
